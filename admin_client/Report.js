189fe304384113b3f6abdf0bbde9f1f3c78e861e
denes <denesgyorgy97@gmail.com>
2022-11-29 18:33:26 -0800

    added submit handler function

--- admin_client/Report.js
+++ admin_client/Report.js
@@ -4,5 +4,12 @@ import axios from 'axios'
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post("http://localhost:6000/login", { username, password })
+            console.log(res.data)
+        }
+    }
 }
