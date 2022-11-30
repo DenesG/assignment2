526a801c23db29812317ff44d04829c34e8a3086
denes <denesgyorgy97@gmail.com>
2022-11-29 18:39:14 -0800

    added html for logging in

--- admin_client/Report.js
+++ admin_client/Report.js
@@ -11,5 +11,35 @@ function Login() {
             const res = await axios.post("http://localhost:6000/login", { username, password })
             console.log(res.data)
         }
+        catch (err) {
+            console.log(err)
+        }
     }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+            <span>Admin Log</span>
+            <br />
+            <input 
+                type="text"
+                placeholder = "username"
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <br />
+            <input
+                type="password"
+                placeholder = "password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <button type="submit">
+                Login
+            </button>
+
+            </form>
+        </div>
+    )
 }
+
+export default Login
