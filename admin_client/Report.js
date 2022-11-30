c35013b0b8705a404d39e99e772e2d5dca1545ee
denes <denesgyorgy97@gmail.com>
2022-11-29 18:45:46 -0800

    added Report.js

--- admin_client/Report.js
+++ admin_client/Report.js
@@ -1,52 +1,65 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import jwt_decode from "jwt-decode";
 
-function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await axios.post("http://localhost:6000/login", { username, password })
-            console.log(res.data)
-        }
-        catch (err) {
-            console.log(err)
-        }
+
+function Report({ id, accessToken, setAccessToken, refreshToken }) {
+  const [reportTable, setReportTable] = useState('');
+  useEffect(() => {
+    const start = async () => {
+      try {
+        const res = await axiosJWT.get(`http://localhost:6001/report?id=${id}`, {
+          headers: {
+            'auth-token-access': accessToken
+          }
+        })
+        setReportTable(res.data);
+      }
+      catch (err) {
+        console.log(err);
+      }
     }
+    start();
+  }, [id]);
 
-    return (
-        <div>
-            {user?.username ? (
-                <>
-                    <h1>Welcome {user.username} </h1>
-                    <Dashboard />
-                </>
-            ) : (
-                <form onSubmit={handleSubmit}>
-            <span>Admin Log</span>
-            <br />
-            <input 
-                type="text"
-                placeholder = "username"
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <br />
-            <input
-                type="password"
-                placeholder = "password"
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <br />
-            <button type="submit">
-                Login
-            </button>
+  const refreshAccessToken = async () => {
+    try {
+      const res = await axios.post("http://localhost:5000/requestNewAccessToken", {}, {
+        headers: {
+          'auth-token-refresh': refreshToken
+        }
+      });
+      // setAccessToken(res.headers['auth-token-access']);
+      return res.headers['auth-token-access']
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-            </form>
-            )}
-        </div>
-    )
+  const axiosJWT = axios.create()
+
+  axiosJWT.interceptors.request.use(
+    async (config) => {
+      let currentDate = new Date();
+      const decodedToken = jwt_decode(accessToken);
+      if (decodedToken.exp * 1000 < currentDate.getTime()) {
+        const newAccessToken = await refreshAccessToken();
+        config.headers["auth-token-access"] = newAccessToken;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  return (
+    <>
+      <div>Report {id && id}</div>
+      <div> {reportTable && reportTable}</div>
+    </>
+  )
 }
 
-export default Login
+export default Report
